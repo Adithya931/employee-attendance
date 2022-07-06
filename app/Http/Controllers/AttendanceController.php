f36f0fe6ee65e4220335b306bf3f5b1d5efac42c
Nidhish PP <nidhishpp.pp@gmail.com>
2022-07-06 12:32:34 +0530

    attenadnce store

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Exception;
+use Carbon\Carbon;
 use App\Models\Employee;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Aws\Rekognition\RekognitionClient;
 
 class AttendanceController extends Controller
@@ -22,12 +24,15 @@ class AttendanceController extends Controller
     public function check(Request $request)
     {
         $request->validate([
-            'image'          => 'required',
+            'image'       => 'required',
+            // 'employee_id' => ['required', 'exists:employees,id'],
+            'mark' => ['required', 'boolean'],
         ]);
 
         $image = fopen($request->file('image')->getPathName(), 'r');
         $bytes = fread($image, $request->file('image')->getSize());
 
+        DB::beginTransaction();
         try {
             $client = new RekognitionClient(config('aws.recognition'));
 
@@ -44,26 +49,63 @@ class AttendanceController extends Controller
             $similarity = $result['FaceMatches'][0]['Similarity'];
             $employee_id = $result['FaceMatches'][0]['Face']['ExternalImageId'];
 
-            $guard = Employee::where('employee_id', $employee_id)->first();
-            return $guard;
+            $employee = Employee::where('employee_id', $employee_id)->first();
+
+            if ($request->mark) {
+                $data = $this->store($employee);
+                DB::commit();
+
+                if ($employee->status == "pending") {
+                    return response()->json([
+                        'message'  => "Employee Check In to work",
+                        'employee'       => $employee,
+                        'status'      => 'success'
+                    ], 200);
+                } else if ($employee->status == "on-duty") {
+                    return response()->json([
+                        'message'  => "Employee Check Out of work",
+                        'employee'       => $employee,
+                        'status'      => 'success'
+                    ], 200);
+                }
+
+                return response()->json([
+                    'message'  => "Employee work already completed",
+                    'employee'       => $employee,
+                    'status'      => 'success'
+                ], 200);
+            }
+
+            return response()->json([
+                'similarity'  => $similarity,
+                'employee'       => $employee,
+                'status'      => 'success'
+            ], 200);
         } catch (Exception $ex) {
 
+            DB::rollback();
             return response()->json([
                 'Message'      => 'Person not Found',
                 'status'       => 'error',
                 'exception' => $ex
             ], 500);
         }
-
-        return response()->json([
-            'similarity'  => $similarity,
-            'guard'       => $guard,
-            'status'      => 'success'
-        ], 200);
     }
 
-    public function store(Request $request)
+    public function store(Employee $employee)
     {
-        # code...
+
+        if ($employee->status == "pending") {
+            $employee->attendances()->create([
+                "check_in" => Carbon::now()
+            ]);
+        } else if ($employee->status == "on-duty") {
+            $attendance = $employee->attendances->where('check_in', Carbon::today())->first();
+            $attendance->update([
+                "check_out" => Carbon::now()
+            ]);
+        }
+
+        return $employee;
     }
 }
